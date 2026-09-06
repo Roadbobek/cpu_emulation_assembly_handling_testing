c6da4ee3fe900364e76d869b93603909d5a511f1
Roadbobek <Roadbobek1234@gmail.com>
2026-09-06 23:12:34 +1000

    study

--- misc/learning_bitwise_operations.c
+++ misc/learning_bitwise_operations.c
@@ -1,12 +1,23 @@
 //
 // Created by Roadb on 6/09/2026.
 // first c code ong
+// long ago i did a very shitty c++ course on w3school or wtv so i think i can do c with no other knowledge.
+// gonna try and do this because ai told me i should learn this before trying to code an assembly interpreter.
+// https://www.geeksforgeeks.org/c/bitwise-operators-in-c-cpp/
+// i understand that a bitwise operation is just an operation directly on the individual bits of some data, either actual bits or the bits of an integer for eg,
+// if im correct it is somewhat what the actual cpu it is very efficient, a very low form of data manipulation.
+// ive heard of the operations but ive never really used them in depth so im gonna practice them here alongside general c.
+// for general things i dont know i will refer to https://www.learn-c.org/ mainly, for eg in the geeksforgeeks bitwise operators tutorial
+// they use formating in the printf function which i dont understand.
+// well they dont really explain everything in depth since its fore begginers so i will juse search it up and use geeksforgeeks for eg,
+//https://www.geeksforgeeks.org/c/printf-in-c/ + https://www.geeksforgeeks.org/c/format-specifiers-in-c/
+//
 //
 
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hi");
+    printf("%c\n", 97);
     return 0;
 }
